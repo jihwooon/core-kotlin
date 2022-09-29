ea239cc9fe12a7602d2236b7e23a9403a825eb90
jihwooon <jihwooon@gmail.com>
2022-09-29 23:35:15 +0900

    [백준] 2439번 별 찍기2

--- app/src/test/kotlin/core/kotlin/backjun/구구단.kt
+++ app/src/test/kotlin/core/kotlin/backjun/구구단.kt
@@ -3,7 +3,7 @@ package core.kotlin.backjun
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-class Gugudan {
+class 구구단 {
     private fun solution(n : Int): Int {
 
         for(i in 1 .. 9) println("$n * $i = ${n * i}")
